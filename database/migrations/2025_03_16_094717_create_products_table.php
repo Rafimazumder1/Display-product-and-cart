c63a945b93708613db918e054d7769e2cb09f26e
Rafimazumder1 <rafimazumder087@gmail.com>
2025-03-16 15:59:59 +0600

    create model and migratrion for product

--- database/migrations/2025_03_16_094717_create_products_table.php
+++ database/migrations/2025_03_16_094717_create_products_table.php
@@ -9,10 +9,14 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
+    public function up()
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string("name", 255)->nullable();
+            $table->text("description")->nullable();
+            $table->string("image", 255)->nullable();
+            $table->decimal("price", 6, 2);
             $table->timestamps();
         });
     }
